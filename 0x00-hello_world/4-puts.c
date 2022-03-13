57c2a45aa7f75cb4f593da6e6e9a99ba3bcad6f3
root <carenjambi@gmail.com>
2022-03-13 08:15:43 -0700

    my 4 puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,17 @@
-#!/bin/bash
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main-Entry point
+ *main -Entry point
  *
- *Return-Always 0 (Success)
+ *Description:uses puts to output
+ *
+ *"programming is like building a multilingual puzzle"
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+char *holbertonschool = " \"programming is like building a multilingual puzzle
+
+puts(holbertonStr);
 return(0);
 }
