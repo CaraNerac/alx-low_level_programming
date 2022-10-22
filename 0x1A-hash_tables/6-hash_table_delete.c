96a0a247028323fc0502f3c7267be56655ef8fd0
root <carenjambi.com>
2022-10-22 07:06:54 -0700

    6 hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,30 +1,33 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - Deletes a hash table.
- * @ht: A pointer to a hash table.
+ * hash_table_delete - deletes a hash table
+ *
+ * @ht: the hash table
+ *
+ * Return: nothing
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_table_t *head = ht;
-	hash_node_t *node, *tmp;
 	unsigned long int i;
+	hash_table_t *hash_table = ht;
+	hash_node_t *hash_node, *tmp;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			node = ht->array[i];
-			while (node != NULL)
+			hash_node = ht->array[i];
+			while (hash_node != NULL)
 			{
-				tmp = node->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				node = tmp;
+				tmp = hash_node->next;
+				free(hash_node->key);
+				free(hash_node->value);
+				free(hash_node);
+				hash_node = tmp;
 			}
 		}
 	}
-	free(head->array);
-	free(head);
+	free(hash_table->array);
+	free(hash_table);
 }
